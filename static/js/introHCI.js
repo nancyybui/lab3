9dd09d88db51e52d1f4f075656edb8723dfdd27a
Nancy Bui <n6bui@ucsd.edu>
2018-01-25 10:23:10 -0800

    thank you nathan

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,28 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("The best way is just to observe the noise of the world.");
+		$("#testjs").toggleClass("active");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
-
+	$("a.thumbnail").click(projectClick);
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#f0f0f0");
+     var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+		$(description).toggle("<div class='project-description'><p>Description of the project.</p></div>");
+      // description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
 }
